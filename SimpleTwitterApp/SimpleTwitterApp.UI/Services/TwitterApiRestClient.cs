dba58310c432d20fc157d9b2c59e9e7985d01131
YOGARAJS <YOGARAJS@outlook.com>
2018-08-23 15:55:06 +0530

    Added MVC

---
+++ SimpleTwitterApp/SimpleTwitterApp.UI/Services/TwitterApiRestClient.cs
@@ -0,0 +1,28 @@
+﻿using SimpleTwitterApp.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleTwitterApp.UI.Services
+{
+    public interface ITwitterApiRestClient
+    {
+        void AddTweet<T>(T model);
+
+        IEnumerable<T> GetAll<T>();
+    }
+
+    public class TwitterApiRestClient : ITwitterApiRestClient
+    {
+        public void AddTweet<T>(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
